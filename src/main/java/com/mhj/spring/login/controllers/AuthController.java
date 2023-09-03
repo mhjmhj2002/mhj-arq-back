c37e902f06499af2141bed4c7b87eb3eb8c2ead7
mhjmhj2002 <mhjmhj2002@gmail.com>
2023-09-02 21:41:14 -0300

    Update AuthController.java

--- src/main/java/com/mhj/spring/login/controllers/AuthController.java
+++ src/main/java/com/mhj/spring/login/controllers/AuthController.java
@@ -37,7 +37,7 @@ import com.mhj.spring.login.security.services.UserDetailsImpl;
 //for Angular Client (withCredentials)
 //@CrossOrigin(origins = "https://mhj-arq-back-production.up.railway.app", maxAge = 3600, allowCredentials="true")
 //@CrossOrigin(origins = "http://localhost:8081", maxAge = 3600, allowCredentials="true")
-@CrossOrigin(origins = "*", maxAge = 3600, allowCredentials="true")
+@CrossOrigin(origins = "*", maxAge = 3600)
 //@CrossOrigin(origins = "http://localhost:4200", maxAge = 3600)
 @RestController
 @RequestMapping("/api/auth")
